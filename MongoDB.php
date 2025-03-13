a1e97aca02f4e3a83606eb6233af0976a1729a10
a22eriwi <a22eriwi@student.his.se>
2025-03-13 17:04:15 +0100

    Updated MongoDB.php file

--- MongoDB.php
+++ MongoDB.php
@@ -1,17 +1,22 @@
 <?php
 require '../vendor/autoload.php';
 
-use MongoDB\Client;
-
 // Connect to MongoDB
-$mongoClient = new Client("mongodb://localhost:27017");
+$mongoClient = new MongoDB\Client("mongodb://localhost:27017");
 
-// Select database and collection
-$database = $mongoClient->local;
-$collection = $database->startup_log;
+$database = $mongoClient->Reddit;
+$collection = $database->RedditData;
 
-$documents = $collection->find();
+//Create empty erray that recieves the documents from collection
+$documents = [];
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['search'])) {
+    $search = trim($_POST['search']);
+
+    $documents = iterator_to_array($collection->find([
+        '$text' => ['$search' => $search]
+    ]));
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,31 +28,48 @@ $documents = $collection->find();
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-
+<div class="centrera">
+    <h1><a href="home.html">Home</a></h1>
+</div>
 <div class="sok">
     <div class="centrera">
-        <h1 class="dbNamn">Search MongoDB</h1>
-        <form action="/search" method="get">
+        <h1 class="dbNamn"><a href="MongoDB.php" id="noUnderline">Search MongoDB</a></h1>
+        <form action="" method="post">
             <input id="searchBar" type="text" name="search" placeholder="Search...">
             <button id="searchButton" type="submit">Search</button>
         </form>
     </div>
 </div>
 
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Data</th>
-    </tr>
-
-    <?php foreach ($documents as $doc): ?>
+<?php
+if (!empty($documents) && count($documents) > 0) {
+?>
+<div id="centerTable">
+    <table>
         <tr>
-            <td><?php echo isset($doc['_id']) ? $doc['_id'] : 'N/A'; ?></td>
-            <td><?php echo isset($doc['hostname']) ? $doc['hostname'] : 'N/A'; ?></td> <!-- Change 'name' to your field -->
+            <th>Subbreddit</th>
+            <th>creation_date</th>
+            <th>Upvotes</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th>num_comments</th>
+            <th>URL</th>
         </tr>
-    <?php endforeach; ?>
 
-</table>
+        <?php foreach ($documents as $doc): ?>
+            <tr>
+                <td><?php echo isset($doc['Subreddit']) ? htmlspecialchars((string)$doc['Subreddit']) : 'N/A'; ?></td>
+                <td><?php echo isset($doc['creation_date'])? htmlspecialchars($doc['creation_date']) : 'N/A'; ?></td>
+                <td><?php echo isset($doc['Upvotes']) ? htmlspecialchars($doc['Upvotes']) : 'N/A'; ?></td>
+                <td><?php echo isset($doc['Title']) ? htmlspecialchars($doc['Title']) : 'N/A'; ?></td>
+                <td><?php echo isset($doc['Body']) ? htmlspecialchars($doc['Body']) : 'N/A'; ?></td>
+                <td><?php echo isset($doc['num_comments']) ? htmlspecialchars($doc['num_comments']) : 'N/A'; ?></td>
+                <td><?php echo isset($doc['URL']) ? htmlspecialchars($doc['URL']) : 'N/A'; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
+<?php } ?>
 
 </body>
 </html>
