a4acb3cd9740f2c65952dddf112d4a6740663b94
Erkan <a22eriwi@student.his.se>
2025-03-24 14:49:27 +0100

    Updated Measurement Script

--- measureScript.js
+++ measureScript.js
@@ -8,9 +8,10 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-//Set measurent number
-const measurmentCycles = 10;
+//Set total measurements
+const totalMeasurements = 10;
 let i = 0;
+let allMeasurements = 0;
 
 //Create array with subreddits
 const subreddits = ["AdviceForTeens", "Anxiety"];
@@ -20,19 +21,32 @@ const searchBar = document.getElementById("searchBar");
 //Create variable form and define it as the searchForm
 const form = document.getElementById("searchForm");
 
-while (measurmentCycles < i ){
+while (i < totalMeasurements ){
     console.log(subreddits[i % subreddits.length]); 
 
+    //Save the current element in the array (subbreddit) to a variable
+    let subreddit = (subreddits[i % subreddits.length])
+    console.log(subreddit);
+
     //Set the value of the searchBar to the current element in array
-    searchBar.value = (subreddits[i % subreddits.length]); 
+    searchBar.value = subreddit; 
 
     // Start timer and save it to var 'measurement1'
-    let measurement1 = performance.timeOrigin + performance.now();
+    let measurement1 = performance.now();
 
     // Submit the form
     form.submit();
 
     // Stop timer and save it to var 'measurement2'
-    let measurement2 = performance.timeOrigin + performance.now();
+    let measurement2 = performance.now();
+
+    let measurementTime = measurement2 - measurement1;
+    let measurementData = subreddit + "," + measurementTime;
+    allMeasurements += measurementTime;
+
+    console.log(measurementData);
+    console.log(allMeasurements);
+
+    i++;
 }
 
