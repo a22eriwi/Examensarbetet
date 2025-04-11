faefe7b76ca77392199cfd69ffd19d1e25ea7f25
a22eriwi <a22eriwi@student.his.se>
2025-04-11 14:07:54 +0200

    Added seed and random function
    Added seed and randomization function to the measurement script in order to randomize the order of the array.

--- measureScript.js
+++ measureScript.js
@@ -4,24 +4,55 @@
 // @version      2024-09-27
 // @description  measureScript
 // @author       You
-// @match        http://localhost/examensarbete/MongoDB.php
-// @match        http://localhost/examensarbete/Couchbase.php
-// @match        http://localhost/examensarbete/MySQL.php
+// @match        http://localhost/examensarbetet/MongoDB.php
+// @match        http://localhost/examensarbetet/Couchbase.php
+// @match        http://localhost/examensarbetet/MySQL.php
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (function () {
     'use strict';
 
+    const seed = 42423;
+
+    //Seed
+    function mulberry32(seed) {
+        return function() {
+            let t = seed += 0x6D2B79F5;
+            t ^= t + Math.imul(t ^ t >>>15, t | 1);
+            t ^= t + Math.imul(t ^ t >>>7, t | 61);
+            return((t ^ t >>> 14) >>> 0) /4294967296;
+        };
+    }
+
+    //Randomize array function
+    function randomizeArray(array, seed) {
+        const random = mulberry32(seed);
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--){
+            const j = Math.floor(random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
+    }
+
     //Set total measurements
-    const totalMeasurements = 10;
+    const totalMeasurements = 28;
 
     //Create two variables, one for the current measurement cycle and one that has all the measurements
     let measurementIndex = parseInt(localStorage.getItem("measurementIndex") || "0");
     let allMeasurements = localStorage.getItem("allMeasurements") || "";
 
     //Create array with subreddits
-    const subreddits = ["AdviceForTeens", "Anxiety"];
+    const subreddits = ["AdviceForTeens", "Anxiety", "ApplyingToCollege", "astrology", "business", "careerguidance", "college", "Colombia", "computer_science", "COVID19", "dating", "depression", "employment", "engineering", "Entrepreneur", "feminism", "GradSchool", "kidsrights", "Mexico_spanishlanguage", "NeutralPolitics", "povertyfinance", "science", "SuicideWatch", "teenagers", "whatsbotheringyou", "YouthandGovernment", "youthshouldknow", "YouthRights"];
+
+    let order = JSON.parse(localStorage.getItem("order") || null);
+
+    //Randomize the subreddits with the seed
+    if (!order) {
+        order = randomizeArray(subreddits, seed);
+        localStorage.setItem("order", JSON.stringify(order));
+    }
 
     //All code inside this function will only execute when the page is fully loaded
     window.addEventListener("load", function () {
@@ -30,7 +61,7 @@
         const searchBar = document.getElementById("searchBar");
         const form = document.getElementById("searchForm");
 
-        //Create variables in order to retrieve from localStorage 
+        //Create variables in order to retrieve from localStorage
         let storedStartTime = parseFloat(localStorage.getItem("measurementStartTime") || "0");
         let storedSubreddit = localStorage.getItem("measurementSubreddit");
 
@@ -41,7 +72,7 @@
             //Calculate the difference between latest and first timestamp
             let measurementTime = measurement2 - storedStartTime;
 
-            //Create a string that contains stordedSubbreddit and the mea
+            //Create a string that contains stordedSubbreddit and the measurement time
             let measurementData = `${storedSubreddit},${measurementTime.toFixed(2)}\n`;
             allMeasurements += measurementData;
 
@@ -56,7 +87,7 @@
         }
         if (measurementIndex < totalMeasurements) {
             //Save the current element in the array (subbreddit) to a variable
-            let subreddit = (subreddits[measurementIndex % subreddits.length]);
+            let subreddit = (order[measurementIndex % order.length]);
 
             //Set the value of the searchBar to the current element in array
             searchBar.value = subreddit;
@@ -71,7 +102,7 @@
             form.submit();
         }
         else {
-            // Make anchor and click it
+            // Save the measurements to CSV file and download it
             var anchor = document.createElement("a");
             var dataBlob = new Blob([allMeasurements], { type: "text/csv" });
             var objUrl = URL.createObjectURL(dataBlob);
